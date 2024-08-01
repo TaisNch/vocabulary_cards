f2fd08ee778f8ef9f3778ad1b75b99bf9920baa4
TaisNch <tais.aliexpress@gmail.com>
2024-08-01 12:51:07 +0200

    slider

--- vocabulary-cards/src/components/AllCards.jsx
+++ vocabulary-cards/src/components/AllCards.jsx
@@ -2,22 +2,32 @@ import Header from './header/Header';
 import Card from './Card/Card';
 import './Card/Card.css';
 import data from '../data.json';
+import { useState } from 'react';
 
 function AllCards () {
-    return (
+  const [currentIndex, setCurrentIndex] =useState(0);
+  
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? data.length - 1 : prevIndex - 1));
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === data.length - 1 ? 0 : prevIndex + 1));
+  };
+  return (
       <div>
          <Header/>
  <h2> Карточки слов</h2>
+ <button onClick={handlePrev}> previous </button>
    <div className='cards'>
-     { data.map((item) => (
-       <Card
-             key={item.id}    
-             word={item.word}
-             transcription={item.transcription}
-             translate={item.translate}
+          <Card
+             key={data[currentIndex].id}    
+             word={data[currentIndex].word}
+             transcription={data[currentIndex].transcription}
+             translate={data[currentIndex].translate}
                               />
-                          ))}
-                          </div>
+                                          </div>
+        <button onClick={handleNext}> next </button>
     </div>
    
       )
--- vocabulary-cards/src/components/Card/Card.jsx
+++ vocabulary-cards/src/components/Card/Card.jsx
@@ -1,21 +1,23 @@
-import './Card.css'
+import './Card.css';
 import { useState } from 'react';
 
-function Card(allLines) {
-  const [active, setActive] = useState(false);
-  
-  const handleActive = () => {
-       const element = document.querySelector(".trslt ");
-       element.classList.toggle("hide");
-  }
-const  {word, transcription, translate}= allLines;
+function Card({ word, transcription, translate }) {
+const [isActive, setIsActive] = useState(false);
+
+const handleActive = () => {
+setIsActive(!isActive);
+}
+
 return (
 <div className='card'>
-       <p className='card__line'>{word}</p>
-       <p className='card__line'>{transcription}</p>
-       <p className='card__line trslt hide'>{translate}</p>
-       <button className='card__button' onClick={handleActive}> Show Transcription</button>
+<p className='card__line'>{word}</p>
+<p className='card__line'>{transcription}</p>
+<p className={`card__line trslt ${isActive ? '' : 'hide'}`}>{translate}</p>
+<button className='card__button' onClick={handleActive}>
+{isActive ? 'Hide Translation' : 'Show Translation'}
+</button>
 </div>
 );
 }
+
 export default Card;
